f1e9432902752c4761c4ce1f8be9b6c52e376644
paolabay <mpbayonalatorre@gmail.com>
2020-08-23 23:47:18 -0500

    cambio

--- service-worker.js
+++ service-worker.js
@@ -12,6 +12,21 @@ var filesToCache = [
     '/',
 ];
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+import {registerRoute} from 'workbox-routing';
+import {NetworkFirst} from 'workbox-strategies';
+
+registerRoute(
+    ({request}) => request.destination === 'script',
+    new NetworkFirst()
+);
 
 self.respondWith(
     fetch(evt.request)
