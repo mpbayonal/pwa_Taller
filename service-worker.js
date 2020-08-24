3f56b9cc6b5e77098c0a5d446005615bf54afa15
paolabay <mpbayonalatorre@gmail.com>
2020-08-23 22:10:27 -0500

    cambio

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,47 @@
 var cacheName = 'shell-content';
 var filesToCache = [
-    '/css/styles.css',
-    '/js/scripts.js',
+    '/styles/inline.css',
+    '/',
+    '/offline.html',
+    '/index.html',
+    '/scripts/app.js',
     '/images/logo.svg',
 
     '/offline.html',
 
     '/',
 ];
+
+
+self.respondWith(
+    fetch(evt.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                    return cache.match('offline.html');
+                });
+        })
+);
+
+self.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+        console.log('[ServiceWorker] Pre-caching offline page');
+        return cache.addAll(FILES_TO_CACHE);
+    })
+);
+
+self.waitUntil(
+    caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+                console.log('[ServiceWorker] Removing old cache', key);
+                return caches.delete(key);
+            }
+        }));
+    })
+);
+
+
 
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
